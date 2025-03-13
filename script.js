136ee6c35f039dc4f4e128fe2c679f3e6ddd72a6
Eduardo Henrique <eduhn1308@gmail.com>
2025-03-13 16:16:01 -0300

    Adiciona favicon e campo para data de validade no formulário

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function analisar() {
     var numeroC = document.querySelector("#number")
     var cvvC = document.querySelector("#cvv")
     var senhaC = document.querySelector("#password")
+    var dataC = document.querySelector("#data")
     var resultado = document.querySelector("#resultado")
 
     var numeroC = Number(numeroC.value)
     var cvvC = Number(cvvC.value)
     var senhaC = Number(senhaC.value)
+    var dataC = dataC.value
 
-        resultado.innerHTML = (`Seu cartão n° ${numeroC}, cvv: ${cvvC} e senha: ${senhaC} <strong>foi clonado!!</strong><br>Para desclonar mande R$ 500,00 na chave pix: <a href="12 34567-8910">12 34567-8910</a>`)
+        resultado.innerHTML = (`Seu cartão n° ${numeroC}, cvv: ${cvvC}, data de validade: ${dataC} e senha: ${senhaC} <strong>foi clonado!!</strong><br>Para desclonar mande R$ 500,00 na chave pix: <a href="12 34567-8910">12 34567-8910</a>`)
     }
