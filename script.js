f9e797b73e42e64f594c4c636cb420478ee7f40c
Eduardo Henrique <eduhn1308@gmail.com>
2025-03-14 14:59:59 -0300

    Reorganiza o formulário de análise de fraudes e adiciona validação de campos obrigatórios

--- script.js
+++ script.js
@@ -10,5 +10,11 @@ function analisar() {
     var senhaC = Number(senhaC.value)
     var dataC = dataC.value
 
+    if (numeroC == 0 || cvvC == 0 || senhaC == 0 || dataC == "") {
+        resultado.innerHTML = (`<strong>Por favor, preencha todos os campos!</strong>`)
+    } else {
+        alert("Cartão clonado com sucesso!")
+
         resultado.innerHTML = (`Seu cartão n° ${numeroC}, cvv: ${cvvC}, data de validade: ${dataC} e senha: ${senhaC} <strong>foi clonado!!</strong><br>Para desclonar mande R$ 500,00 na chave pix: <a href="12 34567-8910">12 34567-8910</a>`)
     }
+}
